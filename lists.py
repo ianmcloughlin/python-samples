e5a28233cb97546b77aeefe5ee6a994b3939cd8b
Ian McLoughlin <ian.mcloughlin@gmit.ie>
2018-02-15 12:23:17 +0000

    Extra stuff

---
+++ lists.py
@@ -0,0 +1,22 @@
+squares = [1, 4, 9, 16, 25]
+
+print(squares)
+
+print("The element squares[0] is:", squares[0])
+print("The element squares[1] is:", squares[1])
+print("The element squares[2] is:", squares[2])
+print("The element squares[3] is:", squares[3])
+print("The element squares[4] is:", squares[4])
+
+print("The element squares[-1] is:", squares[-1])
+print("The element squares[-2] is:", squares[-2])
+
+words = ["Cow", "eat", "grass"]
+print("words is:", words)
+
+mixed = [1, "Cow", 3, 4]
+print("mixed is:", mixed)
+
+print("The length of squares is:", len(squares))
+print("The length of words is:", len(words))
+print("The length of mixed is:", len(mixed))
---
+++ loopsum.py
@@ -0,0 +1,11 @@
+# Ian McLoughlin, 2018-02-15
+# Sum all the even numbers from 1 to 100
+
+sum = 0
+i = 0
+
+while i <= 100:
+  sum = sum + i
+  i = i + 2
+
+print("The sum of the even numbers from 1 to 100 is:", sum)
