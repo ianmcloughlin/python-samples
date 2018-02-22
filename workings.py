c71020174930bfef84020bc458917cdbe9309278
Ian McLoughlin <ian.mcloughlin@gmit.ie>
2018-02-22 14:50:08 +0000

    Files

---
+++ workings.py
@@ -0,0 +1,18 @@
+i = 6 / 2
+
+print("%0.2f" % i)
+
+j = 0
+
+while j < 10:
+  if j % 3 == 0:
+    print(j)
+    print("It's a multiple of 3.")
+  j = j + 1
+
+print("After the loop:", j)
+print("My name is Ian.")
+
+t = True # Hello Python ignore this.
+
+# Hello Python ignore this.
